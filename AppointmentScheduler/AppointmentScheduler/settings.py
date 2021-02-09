47cf479f98bce39bcc6a582dc60ab152e043e5ae
Chukslord <54282120+Chukslord1@users.noreply.github.com>
2021-02-09 16:41:24 +0100

    Update settings.py

--- AppointmentScheduler/AppointmentScheduler/settings.py
+++ AppointmentScheduler/AppointmentScheduler/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x#ma4h0j0bwk)s&$^l9tlua04p2k907c&7xkmdw^hv8l!_2be@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
